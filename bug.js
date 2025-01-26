06eae5ca65fd8485f2a98e33b1f7d9823acbfc54
BugHunter-X <askzenai@gmail.com>
2025-01-26 09:00:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+function foo(a,b){return a+b;}console.log(foo(1,2)); //3console.log(foo(1,"2")); //12console.log(foo(1,true)); //3console.log(foo(1,null)); //1console.log(foo(1,undefined)); //NaNconsole.log(foo(1,[1,2])); //11,2
